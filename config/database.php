851b879838c42b377a8952591dc8c012eb4235b2
Alain Fresco <alain.fresco@gmail.com>
2019-11-13 11:46:26 +0100

    Update database config

--- config/database.php
+++ config/database.php
@@ -48,6 +48,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'url' => env('JAWSDB_URL', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
