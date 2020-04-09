44e79f53d0925d040826dec118e91a16d3456b9a
Ibrahim Mtebene <imtebene@ihiconnect.local>
2020-04-09 14:48:41 +0300

    saving changes

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         yajra\Datatables\DatatablesServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Package Service Providers...
          */
