d7af6cb19ee23fd9b1cf7d1954014067b764a40b
Ibrahim Mtebene <imtebene@ihiconnect.local>
2020-09-09 18:34:47 +0300

    trying to figure out how to save answers

--- app/MedicalCaseAnswer.php
+++ app/MedicalCaseAnswer.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
-
+use App\Node;
 class MedicalCaseAnswer extends Model implements Auditable
 {
   use \OwenIt\Auditing\Auditable;
@@ -22,15 +22,36 @@ class MedicalCaseAnswer extends Model implements Auditable
     $group_two=["Integer","Float","Date","String"];
     foreach($nodes as $node){
       if(array_key_exists('value_format',$node) && in_array($node['value_format'], $group_one) && array_key_exists($node['answer'],$node['answers'])){
-        dd($node['answers'][$node['answer']]);
+        // dd($node['answers'][$node['answer']]);
         //check if the node exists in the database and if it doesnt,create it
-        
+        $issue_node=Node::getOrCreate($node,$algorithm);
+        //save the answers
+        $answer = Answer::getOrCreate();
+        //We parse answers
+        // $medical_case_answer=Self::getOrCreate();
       }elseif(array_key_exists('value_format',$node) && in_array($node['value_format'], $group_two)){
-        dd($node['value']);
+        $issue_node=Node::getOrCreate($node,$algorithm);
+        // $medical_case_answer=Self::getOrCreate();
       }
     }
   }
 
+  public static function getOrCreate($medical_case,$answer,$value,$node){
+    if($value==null){
+      $medica_case_answer = MedicalCaseAnswer::firstOrCreate(
+        [
+          'medical_case_id'=>$medical_case->id,
+          'answer_id'=>$answer_id
+        ],
+        [
+          'node_id'=>$node
+        ]
+      );
+    }else{
+
+    }
+    return $medica_case_answer;
+  }
   /**
    * Get all audits of one medical case
    * @params $id
