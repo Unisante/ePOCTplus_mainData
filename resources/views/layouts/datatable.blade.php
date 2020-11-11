1ac2af374ec17a0e483a26f5741c70dccb1f55ab
Ibrahim Mtebene <imtebene@ihiconnect.local>
2020-11-11 17:38:43 +0300

    after trying the fix of editing rights

--- resources/views/layouts/datatable.blade.php
+++ resources/views/layouts/datatable.blade.php
@@ -1,5 +1,5 @@
 <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" defer></script>
 <script>
   $(document).ready( function () {
