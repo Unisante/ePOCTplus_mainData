ca3081aa2a4df48106879ee7c4ccf67d20267275
Ibrahim Mtebene <imtebene@ihiconnect.local>
2020-12-03 18:56:53 +0300

    after changing the link in the emails

--- resources/views/emails/forgotPassword.blade.php
+++ resources/views/emails/forgotPassword.blade.php
@@ -4,7 +4,7 @@
     <title>Main Data Tanzania</title>
 </head>
 <body>
-<p>Dear {{$username}}.{{ $body }} <a href="{{url('check_password_reset_token/'.$code)}}">Reset password</a></p>
+<p>Dear {{$username}}.{{ $body }} <a href="{{Request::root() ./.'check_password_reset_token/'.$code)}}">Reset password</a></p>
 <p>Thank you</p>
 </body>
 </html>
