1556a9f63ad061bb3c6b84f376fb1e47b1109c9a
Ibrahim Mtebene <imtebene@ihiconnect.local>
2020-12-03 12:14:34 +0300

    returning the views for medical cases back

---
+++ resources/views/medicalCases/show.blade.php
@@ -0,0 +1,94 @@
+@extends('adminlte::page')
+
+<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+@section('content')
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">
+            <a href="/medicalCases" class="btn btn-outline-dark"> Back</a>
+        <a href="{{route('medicalCasesController.showCaseChanges',[$medicalCase->id])}}" class="btn btn-outline-dark float-right">View This Medical Case Changes</a>
+        </div>
+        <div class="card-body">
+          @if (session('status'))
+          <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+          </div>
+          @endif
+          <div class="row">
+            <div class="col-md-8 offset-md-2">
+
+              <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link active" id="home-tab" data-toggle="tab" href="#medicalCase" role="tab" aria-controls="home" aria-selected="true">Medical Case</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Diagnoses</a>
+                </li>
+                {{-- <li class="nav-item" role="presentation">
+                  <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                </li> --}}
+              </ul>
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="medicalCase" role="tabpanel" aria-labelledby="home-tab">
+                  @if($medicalCase)
+                    <div class="showmdCard mb-2">
+                      <div class="card-header">
+                        <span class="font-weight-bold">Created At: </span>
+                        <span>{{$medicalCase->created_at}}</span>
+                      </div>
+                      <div class="card-header">
+                        <span class="font-weight-bold">Updated At: </span>
+                        <span>{{$medicalCase->created_at}}</span> </div>
+                      <div class="card-header">
+                        <div>
+                          <span class="font-weight-bold">Patient Name:</span>
+                          <span class="border-bottom">
+                            {{$medicalCase->patient->first_name}}
+                            {{$medicalCase->patient->last_name}}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card" style="padding:10px">
+                      @foreach($medicalCaseInfo as $case)
+                      <div class="card">
+                        <div class="card-header">
+                          <span class="font-weight-bold">Label: </span>
+                          <span>{{$case->question->label}}</span>
+                        </div>
+                        <div class="card-header">
+                          <span class="font-weight-bold">Stage: </span>
+                          <span>{{$case->question->stage}}</span>
+                        </div>
+                        <div class="card-header">
+                          <span class="font-weight-bold">Description: </span>
+                          @if($case->question->description)
+                          <span>{{$case->question->description}}</span>
+                          @else
+                          <span>No Description</span>
+                          @endif
+                        </div>
+                        <div class="card-header">
+                          <span class="font-weight-bold">Answer: </span>
+                          <span>{{$case->answer}}</span>
+                        </div>
+                        <a href="{{route('medicalCasesController.medicalCaseQuestion', [$medicalCase->id,$case->question->id])}}" class="btn btn-outline-light">Change Answer</a>
+                      </div>
+                      @endforeach
+                    </div>
+                  @endif
+                </div>
+                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                {{-- <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div> --}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
---
+++ resources/views/medicalCases/showCaseChanges.blade.php
@@ -0,0 +1,60 @@
+@extends('adminlte::page')
+
+@section('content')
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">
+        <a href="{{route('medicalCasesController.show',[$medicalCaseId])}}" class="btn btn-outline-dark"> Back</a>
+        </div>
+        <div class="card-body">
+          @if (session('status'))
+          <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+          </div>
+          @endif
+          <div class="row">
+            @include('layouts.compareModal')
+            @include('layouts.datatable')
+            <div class="col-md-10 offset-md-1">
+              @if($allAudits)
+              <table class="table">
+                <thead>
+                  <th scope="col">SN</th>
+                  <th scope="col">User</th>
+                  <th scope="col">Question</th>
+                  <th scope="col">Old value</th>
+                  <th scope="col">New value</th>
+                  <th scope="col">Event</th>
+                  <th scope="col">Ip address</th>
+                  <th scope="col">Created_at</th>
+                </thead>
+                <tbody>
+                  @foreach($allAudits as $audit)
+                  <tr>
+                    <th scope="row">{{ $loop->index }}</th>
+                    <th class="mr-5">{{$audit['user']}}</th>
+                    <th>{{$audit['question']}}</th>
+                    <th>{{$audit['old_value']}}</th>
+                    <th>{{$audit['new_value']}}</th>
+                    <th>{{$audit['event']}}</th>
+                    <th>{{$audit['ip_address']}}</th>
+                    <th>{{$audit['created_at']}}</th>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+              @else
+              <div id="the_card">
+                <h2>There are no changes to this medical Case</h2>
+              </div>
+              @endif
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
