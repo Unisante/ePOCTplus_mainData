cdf775ed2c659af9a95b903ce992024f67799382
Ibrahim Mtebene <imtebene@ihiconnect.local>
2021-03-12 12:35:21 +0300

    testing facility

--- app/Http/Controllers/FacilitiesController.php
+++ app/Http/Controllers/FacilitiesController.php
@@ -16,12 +16,13 @@ class FacilitiesController extends Controller
       $facilities=HealthFacility::all();
       foreach($facilities as $facility){
         //find all medical cases related to that health facility
-        $latest_case=$facility->medical_cases->sortByDesc('updated_at')->first();
+        $latest_case=$facility->medical_cases->sortByDesc('updated_at')->first()->toArray();
+        // dd($latest_case['updated_at']);
         $number_cases=$facility->medical_cases->count();
         $number_patients=$facility->patients->count();
         $facility->number_patients=$number_patients;
         $facility->number_cases=$number_cases;
-        $facility->last_case_time=$latest_case->updated_at;
+        $facility->last_case_time=$latest_case['updated_at'];
       }
       return view('facilities.index')->with('facilities',$facilities);
     }
