e3b78adc53d1e88d983d6c7e661ced91a049c707
informatique@unisante.ch <informatique@unisante.ch>
2021-05-31 16:35:11 +0200

    more meaningful debug

--- app/Jobs/SaveCase.php
+++ app/Jobs/SaveCase.php
@@ -68,6 +68,7 @@ class SaveCase implements ShouldQueue
       $dataForAlgorithm=array("algorithm_id"=> $this->individualData['algorithm_id'],"version_id"=> $this->individualData['version_id'],);
       $algorithm_n_version=Algorithm::ifOrExists($dataForAlgorithm);
       dump("Algo version:",$algorithm_n_version);
+      dump($algorithm_n_version);
       $patient_key=$this->individualData['patient'];
       if($patient_key['study_id']== $study_id && $this->individualData['isEligible']==$isEligible){
         $nodes=$this->individualData['nodes'];
@@ -102,9 +103,11 @@ class SaveCase implements ShouldQueue
           if ($nodes === null) {
             dump("nodes is null");
           }
-          dump($nodes[$algorithm_n_version['config_data']->parent_in_study_id]['answer']);
+          dump("dumping where value");
+          dump("where value",$nodes[$algorithm_n_version['config_data']->parent_in_study_id]['answer']);
         $existingPatient=Answer::where('medal_c_id',$nodes[$algorithm_n_version['config_data']->parent_in_study_id]['answer'])->first();
-          dump($existingPatient);
+          dump("existing patient");
+          dump("existing patient",$existingPatient);
         }
         if($patient_key['other_uid'] || $senseDuplicate || $existingPatient->label == 'Yes'){
           $duplicate_flag=true;
