8adc40bd8d62996d52860292fbf407854a9bfe73
informatique@unisante.ch <informatique@unisante.ch>
2021-06-02 19:20:56 +0200

    reset original migration

--- database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
+++ database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
@@ -18,10 +18,10 @@ class CreateOauthAuthCodesTable extends Migration
      *
      * @return void
      */
-    //public function __construct()
-    //{
-    //    $this->schema = Schema::connection($this->getConnection());
-    //}
+    public function __construct()
+    {
+        $this->schema = Schema::connection($this->getConnection());
+    }
 
     /**
      * Run the migrations.
@@ -32,11 +32,11 @@ class CreateOauthAuthCodesTable extends Migration
     {
         $this->schema->create('oauth_auth_codes', function (Blueprint $table) {
             $table->string('id', 100)->primary();
-            //$table->unsignedInteger('user_id')->index();
-            //$table->unsignedInteger('client_id');
-            //$table->text('scopes')->nullable();
-            //$table->boolean('revoked');
-            //$table->dateTime('expires_at')->nullable();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->unsignedBigInteger('client_id');
+            $table->text('scopes')->nullable();
+            $table->boolean('revoked');
+            $table->dateTime('expires_at')->nullable();
         });
     }
 
