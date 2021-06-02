80f4fe63121a732792954ebe4f1bf66fa153e500
informatique@unisante.ch <informatique@unisante.ch>
2021-06-02 19:07:31 +0200

    testing custom migration for passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        Passport::ignoreMigrations();
+
     }
 
     /**
