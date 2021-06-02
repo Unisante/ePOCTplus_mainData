65efc3892007cf140fd0c1446a1d19a5739a71ff
informatique@unisante.ch <informatique@unisante.ch>
2021-06-02 16:31:13 +0200

    added manage-device permission middleware to DeviceController

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class DevicesController extends Controller
 {
     //
+
+    public function __construct(){
+      $this->middleware('auth');
+      $this->middleware('permission:manage-devices', ['only' => ['index']]);
+    }
+
     public function index(Request $request){
     	return view('devices.index');
     }
