33193cd109bf88992a44c057c601e9138d8a5a6b
informatique@unisante.ch <informatique@unisante.ch>
2021-06-17 12:22:53 +0200

    changed the way error msg is output

--- app/Jobs/SaveZipCasesJob.php
+++ app/Jobs/SaveZipCasesJob.php
@@ -190,7 +190,8 @@ class SaveZipCasesJob implements ShouldQueue
           Storage::copy($this->zip_storage_path, 'extracted_cases_zip/'.basename($this->zip_storage_path));
           // Storage::Delete($this->zip_storage_path);
         }catch(\Exception $e){
-          var_dump($e);
+          error_log($e);
+          Log::info('savecase',  ['error_saving' => $e]);
           Storage::copy($this->zip_storage_path, 'failed_cases_zip/'.basename($this->zip_storage_path));
         }
     }
