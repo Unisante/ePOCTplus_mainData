5b5de9a2b509adeab186008de00714ce99fda85b
informatique@unisante.ch <informatique@unisante.ch>
2021-06-28 11:40:03 +0200

    modified urls for fetching creator informaiton

--- app/HealthFacility.php
+++ app/HealthFacility.php
@@ -21,7 +21,8 @@ class HealthFacility extends Model
         $curl = curl_init();
         curl_setopt_array($curl, array(
           // CURLOPT_URL => 'https://liwi-test.wavelab.top/api/v1/health_facilities/'.$group_id,
-          CURLOPT_URL => 'https://medalc.unisante.ch/api/v1/health_facilities/'.$group_id,
+          //CURLOPT_URL => 'https://medalc.unisante.ch/api/v1/health_facilities/'.$group_id,
+          CURLOPT_URL => 'https://liwi.wavelab.top/api/v1/versions/'.$version_id,
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => "",
           CURLOPT_MAXREDIRS => 10,
