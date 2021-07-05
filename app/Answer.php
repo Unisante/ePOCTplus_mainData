ae531b38d2f59259451ea681e8525bc8c7c81ac1
Benoît Maillard <benoit-leo.maillard@unisante.ch>
2021-07-05 17:19:06 +0200

    Remove obsolete code from models

--- app/Answer.php
+++ app/Answer.php
@@ -9,27 +9,4 @@ class Answer extends Model implements Auditable
 {
   use \OwenIt\Auditing\Auditable;
   protected $guarded = [];
-
-  /**
-  * Create or get answers
-  * @params $nodeSaved
-  * @params $node
-  * @return void
-  */
-  public static function getOrCreate($nodeSaved,$node){
-    if(array_key_exists('answers',$node)){
-      $answers=isset($node['answers'])?$node['answers']:[];
-      foreach($answers as $answer){
-        $answer_issued = Answer::firstOrCreate(
-          [
-            'medal_c_id' => $answer['id'],
-            'node_id'=>$nodeSaved->id
-          ],
-          [
-            'label' => $answer['label'][env('LANGUAGE')]
-          ]
-        );
-      }
-    }
-  }
 }
--- app/custom_diagnosis.php
+++ app/custom_diagnosis.php
@@ -7,23 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Custom_diagnosis extends Model
 {
   protected $guarded = [];
-  /**
-  * store Custom_diagnosis
-  * @params $diagnoses
-  * @params $medical_case_id
-  * @return void
-  */
-  public static function store($diagnoses,$medical_case_id){
-    foreach($diagnoses as $diagnosis){
-      Custom_diagnosis::firstOrCreate(
-        [
-        'medical_case_id'=>$medical_case_id,
-        'label'=>$diagnosis['label']
-        ],
-        [
-          'drugs'=>implode(',', $diagnosis['drugs']),
-        ]
-      );
-    }
-  }
 }
