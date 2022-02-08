4d9028855a572d7a753a5cc344f90845095cdaa6
Sylvain <sylvain.schaufelberger@unisante.ch>
2022-02-08 17:10:27 +0100

    change left pane order

--- config/adminlte.php
+++ config/adminlte.php
@@ -199,25 +199,6 @@ return [
             'can' => 'Access_Follow_Up_Panel',
         ],
         [
-            'text' => 'Duplicates',
-            'icon' => 'fas fa-fw fa-file',
-            'can' => 'Access_Duplicates_Panel',
-            'submenu' => [
-                [
-                    'text' => 'Patients duplicates',
-                    'url' => '/patients/duplicates',
-                    'icon' => 'fas fa-fw fa-user',
-                    'can' => 'Access_Duplicates_Panel',
-                ],
-                [
-                    'text' => 'Cases duplicates',
-                    'icon' => 'fas fa-fw fa-file',
-                    'url' => '/medical-cases/duplicates',
-                    'can' => 'Access_Duplicates_Panel',
-                ],
-            ],
-        ],
-        [
             'text' => 'Facilities',
             'url' => '/facilities',
             'icon' => 'fas fa-fw fa-cart-plus',
@@ -264,6 +245,25 @@ return [
             'text' => 'Drugs',
             'url' => '/exports/drug_list',
             'can' => 'Access_Drugs_Panel',
+        ],
+        [
+            'text' => 'Duplicates',
+            'icon' => 'fas fa-fw fa-file',
+            'can' => 'Access_Duplicates_Panel',
+            'submenu' => [
+                [
+                    'text' => 'Patients duplicates',
+                    'url' => '/patients/duplicates',
+                    'icon' => 'fas fa-fw fa-user',
+                    'can' => 'Access_Duplicates_Panel',
+                ],
+                [
+                    'text' => 'Cases duplicates',
+                    'icon' => 'fas fa-fw fa-file',
+                    'url' => '/medical-cases/duplicates',
+                    'can' => 'Access_Duplicates_Panel',
+                ],
+            ],
         ],
         [
             'text' => 'Exports',
